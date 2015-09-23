1de80c5e803bce5859377d3295dddad8befd4456
justin talbott <justin@waymondo.com>
2015-09-23 16:10:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+[`key-chord`](http://www.emacswiki.org/emacs/key-chord.el) keyword
+support for [`use-package`](https://github.com/jwiegley/use-package).
+
+<!-- ## Installation and Usage -->
+
+<!-- Recommended install from [MELPA](melpa.milkbox.net) with `M-x -->
+<!-- package-install use-package-chords`. Then require it after `use-package`, i.e.: -->
+
+<!-- ``` elisp -->
+<!-- (eval-when-compile -->
+<!--   (require 'use-package)) -->
+<!-- (use-package use-package-chords -->
+<!--   :config (key-chord-mode 1)) -->
+<!-- ``` -->
+
+With this extension, you can define chords using the `:chords` keyword in the same manner
+as `:bind` and related keywords, using a cons or a list of conses:
+
+``` elisp
+(use-package ace-jump-mode
+  :chords (("jj" . ace-jump-char-mode)
+           ("jk" . ace-jump-word-mode)
+           ("jl" . ace-jump-line-mode)))
+```
---
+++ use-package-chords.el
@@ -0,0 +1,67 @@
+;;; use-package-chords.el --- key-chord keyword for use-package
+
+;; Copyright (C) 2015 justin talbott
+
+;; Author: justin talbott <justin@waymondo.com>
+;; Keywords: convenience, tools, extensions
+;; URL: https://github.com/waymondo/use-package-chords
+;; Version: 0.0.1
+;; Package-Requires: ((use-package "2.0") (bind-key "1.0") (key-chord "0.6"))
+;; Filename: use-package-chords.el
+;;
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(require 'bind-key)
+(require 'key-chord)
+
+;;;###autoload
+(defun bind-chord (chord command &optional keymap)
+  "Bind CHORD to COMMAND in KEYMAP (`global-map' if not passed)."
+  (let ((key1 (logand 255 (aref chord 0)))
+        (key2 (logand 255 (aref chord 1))))
+    (if (eq key1 key2)
+        (bind-key (vector 'key-chord key1 key2) command keymap)
+      (bind-key (vector 'key-chord key1 key2) command keymap)
+      (bind-key (vector 'key-chord key2 key1) command keymap))))
+
+;;;###autoload
+(defmacro bind-chords (&rest args)
+  "Bind multiple chords at once.
+
+Accepts keyword argument:
+:map - a keymap into which the keybindings should be added
+
+The rest of the arguments are conses of keybinding string and a
+function symbol (unquoted)."
+  (let* ((map (plist-get args :map))
+         (maps (if (listp map) map (list map)))
+         (key-bindings (progn
+                         (while (keywordp (car args))
+                           (pop args)
+                           (pop args))
+                         args)))
+    (macroexp-progn
+     (apply
+      #'nconc
+      (mapcar (lambda (form)
+                (if maps
+                    (mapcar
+                     #'(lambda (m)
+                         `(bind-chord ,(car form) ',(cdr form) ,m)) maps)
+                  `((bind-chord ,(car form) ',(cdr form)))))
+              key-bindings)))))
+
+(add-to-list 'use-package-keywords :chords t)
+
+(defalias 'use-package-normalize/:chords 'use-package-normalize-binder)
+
+(defun use-package-handler/:chords (name keyword arg rest state)
+  "Handler for `:chords' keyword in `use-package'."
+  (use-package-handler/:bind name keyword arg rest state 'bind-chords))
+
+(provide 'use-package-chords)
+;;; use-package-chords.el ends here
