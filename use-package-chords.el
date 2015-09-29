cf532093fad6b19d6d1f6a2086c8d756b2e11c10
justin talbott <justin@waymondo.com>
2015-09-29 18:20:10 -0400

    extract bind-chords to its own file

--- use-package-chords.el
+++ use-package-chords.el
@@ -16,45 +16,7 @@
 ;;; Code:
 
 (require 'use-package)
-(require 'bind-key)
-(require 'key-chord)
-
-;;;###autoload
-(defun bind-chord (chord command &optional keymap)
-  "Bind CHORD to COMMAND in KEYMAP (`global-map' if not passed)."
-  (let ((key1 (logand 255 (aref chord 0)))
-        (key2 (logand 255 (aref chord 1))))
-    (if (eq key1 key2)
-        (bind-key (vector 'key-chord key1 key2) command keymap)
-      (bind-key (vector 'key-chord key1 key2) command keymap)
-      (bind-key (vector 'key-chord key2 key1) command keymap))))
-
-;;;###autoload
-(defmacro bind-chords (&rest args)
-  "Bind multiple chords at once.
-
-Accepts keyword argument:
-:map - a keymap into which the keybindings should be added
-
-The rest of the arguments are conses of keybinding string and a
-function symbol (unquoted)."
-  (let* ((map (plist-get args :map))
-         (maps (if (listp map) map (list map)))
-         (key-bindings (progn
-                         (while (keywordp (car args))
-                           (pop args)
-                           (pop args))
-                         args)))
-    (macroexp-progn
-     (apply
-      #'nconc
-      (mapcar (lambda (form)
-                (if maps
-                    (mapcar
-                     #'(lambda (m)
-                         `(bind-chord ,(car form) ',(cdr form) ,m)) maps)
-                  `((bind-chord ,(car form) ',(cdr form)))))
-              key-bindings)))))
+(require 'bind-chords)
 
 (add-to-list 'use-package-keywords :chords t)
 
