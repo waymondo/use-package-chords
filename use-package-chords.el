cbf623c867f911732077b026692f9312401791ad
justin talbott <justin@waymondo.com>
2016-01-07 11:52:52 -0500

    avoid clash with :bind handler when determining autoloads
    thanks for @jordonbiondo for pointing out this workaround

--- use-package-chords.el
+++ use-package-chords.el
@@ -24,7 +24,25 @@
 
 (defun use-package-handler/:chords (name keyword arg rest state)
   "Handler for `:chords' keyword in `use-package'."
-  (use-package-handler/:bind name keyword arg rest state 'bind-chords))
+  (let* ((commands
+          (remq nil (mapcar
+                     #'(lambda (arg) (if (listp arg) (cdr arg) nil))
+                     arg)))
+         (chord-binder
+          (list
+           (cons 'progn
+                 (append
+                  (mapcar
+                   (lambda (command)
+                     `(autoload ',command ,(or (and (stringp name) name)
+                                               (symbol-name name))))
+                   commands)
+                  (mapcar
+                   (lambda (binding) `(bind-chord
+                                       ,(car binding)
+                                       ',(cdr binding)))
+                   arg))))))
+    (use-package-handler/:preface name keyword chord-binder rest state)))
 
 (provide 'use-package-chords)
 ;;; use-package-chords.el ends here
