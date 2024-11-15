a2b16a1e64b19ae9428a6cd8f3e09b8159707a29
justin talbott <justin@waymondo.com>
2024-11-15 17:28:21 -0500

    update from use-package github repo
    use-package now lives in emacs core. this package for the last number of years has lived in the
    use-package repo on github that is out of sync of what now lives in emacs core. this repo is now
    getting resurrected to support these extensions

--- use-package-chords.el
+++ use-package-chords.el
@@ -1,44 +1,54 @@
-;;; use-package-chords.el --- key-chord keyword for use-package
+;;; use-package-chords.el --- key-chord keyword for use-package  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015 justin talbott
+;; Copyright (C) 2015-2022 Free Software Foundation, Inc.
 
-;; Author: justin talbott <justin@waymondo.com>
+;; Author: Justin Talbott <justin@waymondo.com>
 ;; Keywords: convenience, tools, extensions
-;; URL: https://github.com/waymondo/use-package-chords
-;; Version: 0.2
+;; URL: https://github.com/jwiegley/use-package
+;; Version: 0.2.1
 ;; Package-Requires: ((use-package "2.1") (bind-key "1.0") (bind-chord "0.2") (key-chord "0.6"))
 ;; Filename: use-package-chords.el
-;; License: GNU General Public License version 3, or (at your option) any later version
-;;
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
+
+;; The `:chords' keyword allows you to define `key-chord' bindings for
+;; `use-package' declarations in the same manner as the `:bind'
+;; keyword.
 
 ;;; Code:
 
 (require 'use-package)
 (require 'bind-chord)
 
-(add-to-list 'use-package-keywords :chords t)
+;;;###autoload
+(defalias 'use-package-autoloads/:chords 'use-package-autoloads-mode)
 
+;;;###autoload
 (defalias 'use-package-normalize/:chords 'use-package-normalize-binder)
 
-(defun use-package-handler/:chords (name keyword arg rest state)
+;;;###autoload
+(defun use-package-handler/:chords (name _keyword arg rest state)
   "Handler for `:chords' keyword in `use-package'."
-  (let* ((commands (remq nil (mapcar #'(lambda (arg)
-                                        (if (listp arg)
-                                            (cdr arg)
-                                          nil)) arg)))
-         (chord-binder
-          (use-package-concat
-           (use-package-process-keywords name
-             (use-package-sort-keywords
-              (use-package-plist-maybe-put rest :defer t))
-             (use-package-plist-append state :commands commands))
-           `((ignore
-              ,(macroexpand
-                `(bind-chords :package ,name ,@arg)))))))
-    (use-package-handler/:preface name keyword chord-binder rest state)))
+  (use-package-concat
+   (use-package-process-keywords name rest state)
+   `(,(macroexpand
+       `(bind-chords :package ,name ,@arg)))))
+
+(add-to-list 'use-package-keywords :chords)
 
 (provide 'use-package-chords)
+
 ;;; use-package-chords.el ends here
